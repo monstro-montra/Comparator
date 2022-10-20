dc2521a0e9bced25a03a1189ee96c8863e1d90f2
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2022-10-20 04:47:12 -0400

    need to repair sortByRollNo method.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class Main {
 
 
@@ -9,14 +11,33 @@ public class Main {
         students[3] = new Student(502134, "Fiona", "903 Murphy Rd.");
         students[4] = new Student(100385, "Esther", "34 Ohms St." );
 
-        for(int i = 0; i < students.length; i++){
-            System.out.println("Student Number: " + (i + 1)  + ", " +students[i]);
-            System.out.println();
-        }
+        printStudents(students);
 
     }
 
     public void sortByRollNo(Student[] students){
+        //selection sort
+        Comparator<Student> studentComparator;
+        for(int i = 0; i < students.length - 1; i++){
+            int min = i; // minimum index = to the current iteration of the outer loop.
+            for (int j = i + 1; j < students.length; j++) { //for each element, in unsorted part of array
+                if (studentComparator.compare(students[j], students[min])) { //check if element in question is smaller than current minimum
+                    min = j; //update minimum element to be this value in question
+                }
+            }
+            //swap with element at the beginning of sub array
+            double temp = arr[i]; // set value of temp equal to index i
+            arr[i] = arr[min]; //set element i = to element min which was the updated minimum element
+            arr[min] = temp; // set the value of arr @ min = temp
+        }
+
+    }
+
+    public static void printStudents(Student[] students){
+        for(int i = 0; i < students.length; i++){
+            System.out.println("Student Number: " + (i + 1)  + ", " +students[i]);
+            System.out.println();
+        }
 
     }
 }
